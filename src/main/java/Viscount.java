c21ccc79030ae93cc486b2f9487a2e820008c45c
samuelkwik <samuelkwik@ymail.com>
2025-01-19 17:48:32 +0800

    Changed "echo" functionality into list add/view

--- src/main/java/Viscount.java
+++ src/main/java/Viscount.java
@@ -1,8 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class Viscount {
     enum Command {
-        ECHO,
+        ADD,
+        LIST,
         BYE
     }
 
@@ -11,6 +15,7 @@ public class Viscount {
         System.out.println("\t" + text.replaceAll("\n", "\n\t"));
         System.out.println("\t_____________________________________________________________");
     }
+
     public static void startChat() {
         String logo = "    __     ___                           _  \n" +
                 "    \\ \\   / (_)___  ___ ___  _   _ _ __ | |_ \n" +
@@ -34,10 +39,28 @@ public class Viscount {
     }
 
     public static Command parseInput(String input) {
-        if(input.toLowerCase().contains("bye")){
+        if (input.toLowerCase().equals("bye")) {
             return Command.BYE;
+        } else if (input.toLowerCase().equals("list")) {
+            return Command.LIST;
         }
-        return Command.ECHO;
+        return Command.ADD;
+    }
+
+    public static void addTask(String task, ArrayList<String> taskList) {
+        taskList.add(task);
+        displayViscountText("Added \"" + task + "\" to your list!");
+    }
+
+    public static void displayTaskList(ArrayList<String> taskList) {
+        if (taskList.isEmpty()) {
+            displayViscountText("You have no tasks!");
+        } else {
+            String taskListString = IntStream.range(0, taskList.size())
+                    .mapToObj(i -> "\t" + (i + 1) + ". " + taskList.get(i))
+                    .reduce("Here is your list of tasks: ", (s1, s2) -> s1 + "\n" + s2);
+            displayViscountText(taskListString);
+        }
     }
 
     public static void main(String[] args) {
@@ -47,18 +70,22 @@ public class Viscount {
         Scanner scanner = new Scanner(System.in);
         String inputString = "";
         Boolean isChatting = true;
+        ArrayList<String> taskList = new ArrayList<>();
 
-        while(isChatting){
+        while (isChatting) {
             showUserPrompt();
             inputString = scanner.nextLine();
             switch (parseInput(inputString)) {
-            case ECHO:
-                displayViscountText(inputString);
-                break;
-            case BYE:
-                sayGoodbye();
-                isChatting = false;
-                break;
+                case ADD:
+                    addTask(inputString, taskList);
+                    break;
+                case LIST:
+                    displayTaskList(taskList);
+                    break;
+                case BYE:
+                    sayGoodbye();
+                    isChatting = false;
+                    break;
             }
         }
     }
