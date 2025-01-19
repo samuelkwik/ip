ebe8e39198db20b1c228db06bada2aaac6de2ad2
samuelkwik <samuelkwik@ymail.com>
2025-01-19 15:46:52 +0800

    Rename "Duke" to "Viscount"

---
+++ src/main/java/Viscount.java
@@ -0,0 +1,11 @@
+public class Viscount {
+    public static void main(String[] args) {
+        String logo = " __     ___                           _  \n" +
+                " \\ \\   / (_)___  ___ ___  _   _ _ __ | |_ \n" +
+                "  \\ \\ / /| / __|/ __/ _ \\| | | | '_ \\| __|\n" +
+                "   \\ V / | \\__ \\ (_| (_) | |_| | | | | |_ \n" +
+                "    \\_/  |_|___/\\___\\___/ \\__,_|_| |_|\\__|\n";
+
+        System.out.println("Hello from\n" + logo);
+    }
+}
