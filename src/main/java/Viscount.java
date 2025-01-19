8dd8bef7e560ac57a8962c490d74ec1f6e366d62
samuelkwik <samuelkwik@ymail.com>
2025-01-19 16:10:21 +0800

    Add greeting and exit

--- src/main/java/Viscount.java
+++ src/main/java/Viscount.java
@@ -1,5 +1,10 @@
 public class Viscount {
-    public static void main(String[] args) {
+    private static void promptUser(String text) {
+        System.out.println("____________________________________________________________");
+        System.out.println(text);
+        System.out.println("____________________________________________________________");
+    }
+    public static void startChat() {
         String logo = " __     ___                           _  \n" +
                 " \\ \\   / (_)___  ___ ___  _   _ _ __ | |_ \n" +
                 "  \\ \\ / /| / __|/ __/ _ \\| | | | '_ \\| __|\n" +
@@ -7,5 +12,20 @@ public class Viscount {
                 "    \\_/  |_|___/\\___\\___/ \\__,_|_| |_|\\__|\n";
 
         System.out.println("Hello from\n" + logo);
+    }
+
+    public static void sayHello() {
+        promptUser("Hello! I'm Viscount! \n What can I do for you?");
+    }
+
+    public static void sayGoodbye() {
+        promptUser("Bye. Hope to see you again soon!");
+    }
+
+    public static void main(String[] args) {
+        startChat();
+        sayHello();
+        sayGoodbye();
+
     }
 }
