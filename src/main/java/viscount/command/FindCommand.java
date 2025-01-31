53aa0177d7155631695e522d31bea434a849bc26
samuelkwik <samuelkwik@ymail.com>
2025-01-31 13:52:24 +0800

    Add support for find functionality

---
+++ src/main/java/viscount/command/FindCommand.java
@@ -0,0 +1,22 @@
+package viscount.command;
+
+import viscount.Storage;
+import viscount.TextUi;
+import viscount.ViscountException;
+import viscount.task.TaskList;
+
+public class FindCommand extends Command{
+    private final String searchTerm;
+    public FindCommand(String searchTerm) {
+        this.searchTerm = searchTerm;
+    }
+
+    @Override
+    public void execute(TaskList taskList, TextUi textUi, Storage storage) throws ViscountException {
+        String findResults = taskList.getTasksStreamWithIndex()
+                .filter(s -> s.contains(searchTerm))
+                .reduce((s1, s2) -> s1 + "\n" + s2)
+                .orElse("Find: [" + searchTerm + "] : No results found");
+        textUi.displayViscountText(findResults);
+    }
+}
